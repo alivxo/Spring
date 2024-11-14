5b83b77e71e2c5f9e8eca4337765a97801c206ff
Alivxo <avsal0218@gmail.com>
2024-11-13 19:22:45 -0500

    first commit

---
+++ src/main/java/com/ali/QuikMart/QuikMartApplication.java
@@ -0,0 +1,13 @@
+package com.ali.QuikMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuikMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuikMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ali/QuikMart/QuikMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ali.QuikMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuikMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
